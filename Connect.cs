0db794f6d3ccf37cc775725ede43453c91465383
Cs12krisz <csigerkrisztian@gmail.com>
2025-09-19 08:17:50 +0200

    Connect class created

---
+++ Connect.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAdatbazis
+{
+    internal class Connect
+    {
+        public MySqlConnection Connection;
+
+        private string _host;
+        private string _database;
+        private string _user;
+        private string _password;
+        private string _conectionString;
+
+        public Connect(string database)
+        {
+            _host = "localhost";
+            _database = database;
+            _user = "root";
+            _password = "";
+
+            _conectionString = $"Server={_host}; Database={_database}; UID={_user}; Password='{_password}'; SslMode=None;";
+
+            Connection = new MySqlConnection(_conectionString);
+
+            try
+            {
+                Connection.Open();
+                Console.WriteLine("Sikeres csatlakozás");
+                Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
